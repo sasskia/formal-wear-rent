0521677535be079c0282e0fbcbe960c4bac6398a
Har Dhillon <har7173@gmail.com>
2020-11-16 15:05:41 +0000

    routes change

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-  root to: 'pages#home'
-=======
   get "listings", to: "listings#index"
->>>>>>> c247cc4577944ed7708cbfffe2dce292e4e2b94e
 end
