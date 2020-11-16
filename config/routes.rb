4669271bfc697253197e9a3a7e71440d310f562f
Fanny Karlsson <fanny.ll.karlsson@gmail.com>
2020-11-16 15:46:52 +0000

    added validations to all models and route to listings in controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
   root to: 'pages#home'
-  
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "listings", to: "listings#index"
+   resources :listings do
+     member do
+       get :my_listings
+   end
+ end
 end
