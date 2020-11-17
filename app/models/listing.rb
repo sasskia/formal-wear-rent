3cb6071a25f10754a264735b7084e346ceb3848d
Saskia Knauf <saskia.knauf@gmail.com>
2020-11-17 11:00:39 +0000

    seeding created

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,10 +4,11 @@ class Listing < ApplicationRecord
   has_many :reviews
   validates :name, presence: true
   validates :name, length: { maximum: 50 }
-  validates :category, inclusion: { in: %w(Tuxedo,Black tie,Gown,Dress) }
-  validates :gender, inclusion: { in: %w(men,women) }
+  validates :category, inclusion: { in: ["Tuxedo", "Black tie", "Gown", "Dress"] }
+  validates :gender, inclusion: { in: %w(men women) }
   validates :gender, presence: true
   validates :category, presence: true
   validates :description, length: { minimum: 10 }
-  validates :descitption, presence: true
+  validates :description, presence: true
+  validates :size, presence: true
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "formal_wear_rent_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'https://formal-wear-rent.herokuapp.com/' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
