183be7bc55339cbb1b0982d820eaa53cc9211298
Saskia Knauf <saskia.knauf@gmail.com>
2020-11-17 15:06:20 +0000

    add listing created

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -11,4 +11,30 @@ class ListingsController < ApplicationController
   def show
     @listing = Listing.find(params[:id])
   end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(strong_params)
+    @listing.user_id = current_user.id
+    if @listing.save
+      redirect_to listing_path(@listing.id)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+    redirect_to my_listings_path()
+  end
+
+  private
+
+  def strong_params
+    params.require(:listing).permit(:name, :category, :gender, :size, :price_per_night, :description)
+  end
 end
