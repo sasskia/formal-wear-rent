470a8ae0c991baebcaa113ee87ad6cfd5f04f5b5
Matt Daloisio <mattdam1@yahoo.co.uk>
2020-11-17 11:22:25 +0000

    Working Navbar and Landing Page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,12 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap';
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  initUpdateNavbarOnScroll();
+});
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
