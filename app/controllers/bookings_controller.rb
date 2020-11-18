a0a5a008d2392544160a5f4ca7d2e351e16d84cf
Fanny Karlsson <fanny.ll.karlsson@gmail.com>
2020-11-18 12:09:55 +0000

    added confirm feature

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,9 +3,22 @@ class BookingsController < ApplicationController
     @bookings = current_user.bookings
   end
 
+
   def show
     @booking = Booking.find(params[:id])
     @total_price = total_price(@booking)
+  end
+
+  def booking_decline
+    @booking = Booking.find(params[:id])
+    @booking.update(confirmed: false)
+    redirect_to my_listings_path
+  end
+
+  def booking_confirm
+    @booking = Booking.find(params[:id])
+    @booking.update(confirmed: true)
+    redirect_to my_listings_path
   end
 
   def total_price(booking)
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :listings do
   end
-  resources :bookings, only: [ :show, :edit, :update, :create, :new ] do
+  resources :bookings, only: [ :show, :update, :create, :new ] do
   end
 
   get 'my_listings', to: 'listings#my_listings', as: 'my_listings'
   get 'my_bookings', to: 'bookings#my_bookings', as: 'my_bookings'
+  patch 'booking_confirm/:id', to: 'bookings#booking_confirm', as: 'booking_confirm'
+  patch 'booking_decline/:id', to: 'bookings#booking_decline', as: 'booking_decline'
 end
