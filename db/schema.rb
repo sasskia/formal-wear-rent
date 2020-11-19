c95cc21c3d1affd327e3d454fac9a85181151ef6
Fanny Karlsson <fanny.ll.karlsson@gmail.com>
2020-11-19 12:05:25 +0000

    fixed xml file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_105333) do
+ActiveRecord::Schema.define(version: 2020_11_19_114749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
