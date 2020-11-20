fd95fd33d3182971a6d4ba00a927afac9de58af1
Fanny Karlsson <fanny.ll.karlsson@gmail.com>
2020-11-20 11:13:52 +0000

    fixed the validations, confirmed buttons now WORK!

--- app/validators/availability_validator.rb
+++ app/validators/availability_validator.rb
@@ -2,6 +2,8 @@ class AvailabilityValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
     bookings = Booking.where(["listing_id =?", record.listing_id])
+
+    bookings = bookings.map { |b| b }.reject! { |booking| booking == record }
     date_ranges = bookings.map { |b| b.start_date..b.end_date }
 
     date_ranges.each do |range|
