78020692799c60ebcf5c14f0ad5040584c603256
Fanny Karlsson <fanny.ll.karlsson@gmail.com>
2020-11-20 12:09:54 +0000

    fixed validations

--- app/validators/availability_validator.rb
+++ app/validators/availability_validator.rb
@@ -2,15 +2,15 @@ class AvailabilityValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
     bookings = Booking.where(["listing_id =?", record.listing_id])
-      bookings = bookings.map { |b| b }.reject! { |booking| booking == record }
-      date_ranges = bookings.map { |b| b.start_date..b.end_date }
+    bookings = (bookings || []).map { |b| b }.delete_if { |booking| booking == record }
 
-      date_ranges.each do |range|
-        if range.include? value
-          record.errors.add(attribute, "not available")
-        end
+    date_ranges = bookings.map { |b| b.start_date..b.end_date }
+
+    date_ranges.each do |range|
+      if range.include? value
+        record.errors.add(attribute, "not available")
       end
     end
+  end
 end
-
 
